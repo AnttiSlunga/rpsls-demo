019c539ae576dc5b3e5c9a541ea29da8c97ee203
Antti Slunga <antti_slunga@hotmail.com>
2015-09-01 21:25:42 +0300

    Very simple spring boot support

--- src/main/java/com/demo/rpsls/Main.java
+++ src/main/java/com/demo/rpsls/Main.java
@@ -1,26 +1,38 @@
 package com.demo.rpsls;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.api.rpsls.FightResult;
 import com.api.rpsls.GameEngine;
-import com.engine.rpsls.warriors.*;
+import com.engine.rpsls.GameEngineImpl;
+import com.engine.rpsls.warriors.Paper;
+import com.engine.rpsls.warriors.Rock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by antti on 25.4.2015.
  */
+@Controller
+@EnableAutoConfiguration
 public class Main {
 	
 	@Autowired
 	private static GameEngine engine;
-	
-    public static void main(String args[]) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/ApplicationContext.xml");
-        //GameEngine engine = (GameEngine) applicationContext.getBean("engine");
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/ApplicationContext.xml");
+        GameEngine engine = new GameEngineImpl();
         FightResult result = engine.fight(new Paper(), new Rock());
-        System.out.println("Paper : " +result.name());
+        return ("Paper : " +result.name());
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
 
 }
